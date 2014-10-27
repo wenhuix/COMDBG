8712d4b256378e546a252cc25d156909bfa52736
wenhuix <xwh1@mail.ustc.edu.cn>
2014-10-27 22:31:44 +0800

    set text select index to the end

--- COMDBG/COMDBG/View/MainForm.Designer.cs
+++ COMDBG/COMDBG/View/MainForm.Designer.cs
@@ -329,6 +329,7 @@
             this.receivetbx.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.receivetbx.Size = new System.Drawing.Size(329, 152);
             this.receivetbx.TabIndex = 9;
+            this.receivetbx.TabStop = false;
             this.receivetbx.TextChanged += new System.EventHandler(this.receivetbx_TextChanged);
             // 
             // sendtbx
